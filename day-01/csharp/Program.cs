4617b15aab0fac6449a6a671e17f8d07044e7f04
sweetpeteza <peter@playbk.io>
2023-10-19 00:05:10 +0200

    day1

--- day-01/csharp/Program.cs
+++ day-01/csharp/Program.cs
@@ -1,14 +1,32 @@
-﻿using System.Threading.Channels;
+﻿using System.Diagnostics;
+using System.Threading.Channels;
 using day_01;
+
+var sw = Stopwatch.StartNew();
 
 Console.WriteLine("AOC Day 01");
 
 string[] lines = File.ReadAllLines("./input.txt");
 
-Console.WriteLine("Part 1");
+//Console.WriteLine("Part 1");
+//Console.WriteLine("===========");
+//Console.WriteLine(PartOne.CorrectAnswer());
 Console.WriteLine("===========");
-Console.WriteLine(PartOne.CorrectAnswer());
 Console.WriteLine($"Max Elf: {PartOne.GetMaxElf(lines)}");
+sw.Stop();
+
+var sw2 = Stopwatch.StartNew();
+
+Console.WriteLine("===========");
+Console.WriteLine($"Completed in {sw.ElapsedMilliseconds}ms");
 
 Console.WriteLine("===========");
 Console.WriteLine($"Top 3 Elf Total:{PartTwo.GetElfTotals(lines).GetUpperXElfTotal(3)}");
+
+sw2.Stop();
+
+Console.WriteLine("===========");
+Console.WriteLine($"Completed in {sw.ElapsedMilliseconds}ms");
+
+
+
